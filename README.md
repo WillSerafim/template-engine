dcd24c1500e8fc0ace4fb6ff2887817a8fa62222
Willian Serafim <willian_50@ymail.com>
2020-03-22 15:47:28 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Projeto
+
+Projeto realizado com Laravel para mostrar o funcionamento de um Template engine, neste projeto foi utilizado o [Blade](https://laravel.com/docs/7.x/blade#introduction) para demonstração.
+
+## Instalação
+```
+git clone  https://github.com/WillSerafim/template-engine.git
+```
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = [
+            'userName' => 'Willian Serafim',
+            'permissions' => [
+                //all permissions
+                'editName' => 1,
+                'editemail' => 1,
+                'editPassword' => 1,
+                'editArea' => 1,
+
+                //without permissions
+                // 'editName' => 0,
+                // 'editemail' => 0,
+                // 'editPassword' => 0,
+                // 'editArea' => 0,
+
+                //random
+                // 'editName' => rand(0,1),
+                // 'editemail' => rand(0,1),
+                // 'editPassword' => rand(0,1),
+                // 'editArea' => rand(0,1),
+            ],
+            'areas' => [
+                'TI',
+                'RH',
+                'Marketing',
+                'Gerência',
+                'Financeiro',
+                'Comercial',
+                'Manutenção',
+            ]
+
+        ];
+
+        return view('user',['data' => $data]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
